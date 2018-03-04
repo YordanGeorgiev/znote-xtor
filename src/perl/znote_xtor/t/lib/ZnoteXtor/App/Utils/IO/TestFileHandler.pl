9ef5dc6a160cc10a973259470c46431190ed4e27
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-03-04 12:44:59 +0200

    zx-180228 add add the doReadDirGetFilesByExtension method in the FileHandler

--- src/perl/znote_xtor/t/lib/ZnoteXtor/App/Utils/IO/TestFileHandler.pl
+++ src/perl/znote_xtor/t/lib/ZnoteXtor/App/Utils/IO/TestFileHandler.pl
@@ -8,20 +8,33 @@ BEGIN { unshift @INC, "$FindBin::Bin/" }
 
 use ZnoteXtor::App::Utils::Initiator ; 
 use ZnoteXtor::App::Utils::IO::FileHandler ; 
-use Test::More tests => 2 ; 
+use Test::More tests => 3 ; 
 use Data::Printer ; 
 
-
+my $objInitiator 				= 'ZnoteXtor::App::Utils::Initiator'->new(5) ; 
+my $ProductInstanceDir 		= $objInitiator->doResolveMyProductInstanceDir();
+my $appConfig				   = $objInitiator->get ('AppConfig'); 
+p $appConfig ; 
 my $m = () ;            # the msg for each test 
 my $objFileHandler= 'ZnoteXtor::App::Utils::IO::FileHandler'->new();
 
 $m = 'can create the FileHandler obj' ; 
 ok ( defined ( $objFileHandler ) , "$m" );
 
-
-my @methods = ('doMkDir', 'doPrintToFile' , 'doReadFileReturnString' );
+my @methods = ('doMkDir', 'doPrintToFile' , 'doReadFileReturnString' , 'doReadFileReturnArrayRef' );
 $m = 'an objFileHandler has the following methods ' . @methods ; 
 can_ok($objFileHandler, @methods);
+
+my $arrRef = $objFileHandler->doReadDirGetFilesByExtension ( $ProductInstanceDir , 'pm|pl' ); 
+# p $arrRef ; 
+my @sorted_arr = sort @$arrRef ; 
+my $str_cmd_output = `find $ProductInstanceDir -name '*.pl' -o -name '*.pm'` ; 
+my @cmd_arr = sort(split '\n' , $str_cmd_output );
+
+$m = 'the objFileHandler can return the files by extenstion from a dir' ; 
+ok ( @sorted_arr == @cmd_arr , $m ); 
+
+
 
 1;
 
